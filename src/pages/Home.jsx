27cfeef04df8d1872286c7cb53ee8d03676d6a34
Haritha Ganegoda <haritha.99ganegoda@gmail.com>
2025-07-07 15:33:57 +0530

    Initial Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { ThemeToggle } from '../components/ThemeToggele';
+
+const Home = () => {
+  return (
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+        {/* Theme Toggle */}
+        <ThemeToggle />
+
+        {/* Background Effect */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+        {/* Footer */}
+
+    </div>
+   
+  )
+}
+
+export default Home;
